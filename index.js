2782563c7d28576f5644432e240be523d4c36290
trelana <tremartin3003@gmail.com>
2024-02-29 18:46:03 -0600

    Waiting on Slack answer & experimenting with ways to make the username a link.  I think I have to change the already existing var, but want confirmation.

--- index.js
+++ index.js
@@ -12,8 +12,29 @@ $(document).ready(() => {
   // Below is NOT a func: Var thats the result of mapping
   const $tweets = streams.home.map((tweet) => { // Generates a set number of new tweets
     const $tweet = $('<div></div>'); // Creates a div for each individual new tweet
-    const text = `@${tweet.user}: ${tweet.message}`; // Combines username with the message into 1 var
-//-------------------------
+    // Make Username Clickable Start ----------------------------------
+    // How do I select the usernames?  `@${tweet.user} on line 15 is a clue maybe?
+    // Clicking it should show the users timeline, meaning only their posts (I think) where are those?
+    // streams.users has an array of the usernames. Are these their timelines?
+    let $uNSelect = tweet.user
+    // console.log(uNSelect) // This logs the usernames
+    // Do I need to make a new Username div, or am I meant to modify the one that already exists?
+    // Making a tag set for now until someone answers my question.
+    let $unLink = $('<a>'); // Create new anchor element/tag
+    // Make the href attribute link to the users page (use google till I can figure that out)
+    $unLink.attr("href", "https://www.google.com/");
+    // Set the link text between the 'a' tags
+    $unLink.text(tweet.user)
+    // move this block above the const text just in case
+    // Make Username Clickable End ----------------------------------
+
+    const text = `@${tweet.user}: ${tweet.message}`; // Testing
+    // const text = `@${tweet.user}: ${tweet.message}`; // OG: Combines username with the message into a String & assigns it to a var
+    console.log(typeof text) // Ok the lil tics are making this a text string only
+
+    $tweet.text(text); // Adds combo username + msg var to the individual divs
+
+// Timestamp Creation Start-------------------------
 /**
 - Display the timestamps of when the tweets were created.
 This timestamp should reflect the actual time the tweets were created, 
@@ -22,20 +43,27 @@ and should not just be hardcoded.
 You’ll want to use a library to do this work for you.
 A very popular libary is called Moment.js
  */
+// Create timestamp div
+let $timeStampDiv = $('<div></div>'); // Makes a div to put the timestamps in
 let callTime = moment().calendar(); // Exact time post was made
 let pastTime = moment().startOf(callTime).fromNow(); // How much time has passed since posted
-console.log('Moment time: ', pastTime + ': ' + callTime); // Logs what I want!!!
+// console.log('Moment time: ', pastTime + ': ' + callTime); // Logs what I want!!!
+let tweetStamp = pastTime + ': ' + callTime;
+// console.log(tweetStamp)
+// Adds the time var to the div I made for the full time stamp
+$timeStampDiv.text(tweetStamp);
+// Adds the timeStamp div to the tweet div 
+$tweet.append($timeStampDiv)
+// Timestamp Creation End------------------------
 
 
-//------------------------
-    $tweet.text(text); // Adds combo username + msg var to the individual divs
-
-    return $tweet; // returns div contain UN + Msg
+    return $tweet; // returns 2 divs: 1 contains UN + Msg, the 2nd has the timeStamp
   });
-  $tweetsDiv.append($tweets); // NOT A FUNC: Adds 16 tweets at a time to tweetsDiv
+  $tweetsDiv.append($tweets); // NOT A FUNC: Adds 16 tweets (and their timestamps) at a time to tweetsDiv
 }
-
-  // I want a Func Call to add tweets automatically: 1000 milSecs = 1 sec
+// Show Tweets start ---------------------------------------------------
+// Show the user new tweets somehow. (You can show them automatically as they’re created, or create a button that displays new tweets.)
+// I want a Func Call to add tweets automatically: 1000 milSecs = 1 sec
   let addTweets = setInterval(futureFunc, 2000); // Despite the EXs I found showing the () or "" where needed, it only works when just the func name is present
   // Auto showing these may be a bad idea, the page gets ridiculously longafter a while
   // Isnt there a way to stop it?
@@ -44,7 +72,7 @@ console.log('Moment time: ', pastTime + ': ' + callTime); // Logs what I want!!!
     clearInterval(addTweets); // This will stop the adding process
   }
   setInterval(stopTweetTimer, 6000); // This stops the adding process after 6 secs to keep page from flooding
-
+// Show Tweets End -----------------------------------------------------
 
 });
 
